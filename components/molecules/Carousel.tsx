7284bbb1f76da1f5b2bb8fd4ac1c1594cb593d82
ikramakcha <akcha.ikram.a1@gmail.com>
2022-03-26 22:02:26 +0100

    The Carousel Component.

--- components/molecules/Carousel.tsx
+++ components/molecules/Carousel.tsx
@@ -1,10 +1,8 @@
-
 import ArrowNext from '../assets/svgs/Arrow 1.svg';
 import ArrowPrev from '../assets/svgs/Arrow 2.svg';
 import Image from 'next/image';
-import {FC} from 'react';
-import dynamic from 'next/dynamic';
-dynamic(() => import('tw-elements'));
+import {FC, useEffect} from 'react';
+
 const Carousel : FC<{
     id: string;
     title: string;
@@ -13,6 +11,10 @@ const Carousel : FC<{
   id,
   title,
 }) => {
+  useEffect(() => {
+    require('tw-elements');
+  });
+
   return (
     <div id={id} className="carousel slide relative
       bg-carousel bg-cover w-[911px] h-[261px]"
@@ -21,7 +23,7 @@ const Carousel : FC<{
          left-0 flex justify-center p-0 mb-4">
         <button
           type="button"
-          data-bs-target={id}
+          data-bs-target={`#${id}`}
           data-bs-slide-to="0"
           className="active"
           aria-current="true"
@@ -29,15 +31,27 @@ const Carousel : FC<{
         </button>
         <button
           type="button"
-          data-bs-target={id}
+          data-bs-target={`#${id}`}
           data-bs-slide-to="1"
           aria-label="Slide 2" >
         </button>
         <button
           type="button"
-          data-bs-target={id}
+          data-bs-target={`#${id}`}
           data-bs-slide-to="2"
           aria-label="Slide 3" >
+        </button>
+        <button
+          type="button"
+          data-bs-target={`#${id}`}
+          data-bs-slide-to="3"
+          aria-label="Slide 4" >
+        </button>
+        <button
+          type="button"
+          data-bs-target={`#${id}`}
+          data-bs-slide-to="4"
+          aria-label="Slide 5" >
         </button>
       </div>
       <div className="carousel-inner relative w-full overflow-hidden">
@@ -49,6 +63,36 @@ const Carousel : FC<{
                 Notorious Pirate!</span></h1>
             <p className='text-white text-center text-base font-normal'>
               Discover the true meaning of notoriety in the<br/>
+                world’s first upcoming Pirate themed Metaverse</p>
+            <button className='text-black bg-white text-sm font-normal
+                   py-1 px-4 border rounded mt-5'>
+                  Learn More...
+            </button>
+          </div>
+        </div>
+        <div className="carousel-item  relative float-left w-full">
+          <div className="carousel-caption hidden md:block
+              text-center">
+            <h1 className="text-2xl text-white">{title}
+              <span className='text-yellow-600 text-xl block mb-5'>
+                Notorious Pirate!</span></h1>
+            <p className='text-white text-center text-base font-normal'>
+            Discover the true meaning of notoriety in the<br/>
+                world’s first upcoming Pirate themed Metaverse</p>
+            <button className='text-black bg-white text-sm font-normal
+                   py-1 px-4 border rounded mt-5'>
+                  Learn More...
+            </button>
+          </div>
+        </div>
+        <div className="carousel-item  relative float-left w-full">
+          <div className="carousel-caption hidden md:block
+              text-center">
+            <h1 className="text-2xl text-white">{title}
+              <span className='text-yellow-600 text-xl block mb-5'>
+                Notorious Pirate!</span></h1>
+            <p className='text-white text-center text-base font-normal'>
+            Discover the true meaning of notoriety in the<br/>
                 world’s first upcoming Pirate themed Metaverse</p>
             <button className='text-black bg-white text-sm font-normal
                    py-1 px-4 border rounded mt-5'>
@@ -92,7 +136,7 @@ const Carousel : FC<{
          items-center justify-center p-0 text-center border-0 hover:outline-none
           hover:no-underline focus:outline-none focus:no-underline left-0"
         type="button"
-        data-bs-target={id}
+        data-bs-target={`#${id}`}
         data-bs-slide="prev"
       >
         <span className="carousel-control-prev-icon
@@ -106,7 +150,7 @@ const Carousel : FC<{
          items-center justify-center p-0 text-center border-0 hover:outline-none
           hover:no-underline focus:outline-none focus:no-underline right-0"
         type="button"
-        data-bs-target={id}
+        data-bs-target={`#${id}`}
         data-bs-slide="next"
       >
         <span className="carousel-control-next-icon
--- components/templates/Main.tsx
+++ components/templates/Main.tsx
@@ -1,13 +1,7 @@
-
-import Carousel from '../molecules/Carousel';
 const Main = () => {
   return (
-    <div className="container mx-auto justify-center">
-      <Carousel
-        title='Find out what it means to become a'
-        id='CarouselTarget'
-
-      />
+    <div className="container mx-auto">
+      <h1 className="text-3xl font-bold underline">Home Page</h1>
     </div>
   );
 };
