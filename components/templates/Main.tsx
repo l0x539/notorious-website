67b9c080afb7a4ca93c5b520aa6818037deadcc9
ikramakcha <akcha.ikram.a1@gmail.com>
2022-03-27 22:33:27 +0200

    Banner Component

--- components/templates/Main.tsx
+++ components/templates/Main.tsx
@@ -1,7 +1,15 @@
+import NewsBanner from '../molecules/NewsBanner';
+
 const Main = () => {
   return (
     <div className="container mx-auto">
-      <h1 className="text-3xl font-bold underline">Home Page</h1>
+      <NewsBanner
+        title='Find out what it means to become a'
+        description=' world’s first upcoming Pirate themed Metaverse'
+        time='2022-01-21 17:00:00'
+        comments={3}
+
+      />
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
+
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./layout/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './layout/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        banner: 'url(\'../components/assets/images/banner-bg.png\')',
+      }),
+      boxShadow: {
+        inner: 'inset 0px 4px 118px 48px rgba(0, 0, 0, 0.75)',
+      },
+    },
   },
   plugins: [],
-}
+};
