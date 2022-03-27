270e04d46e737c122cba6b9469dee9cc9a20ba69
Akcha Ikram <86748607+ikramakcha@users.noreply.github.com>
2022-03-27 17:17:28 +0200

    Update Main.tsx

--- components/templates/Main.tsx
+++ components/templates/Main.tsx
@@ -1,9 +1,15 @@
+import Carousel from '../molecules/Carousel';
 const Main = () => {
   return (
-    <div className="container mx-auto">
-      <h1 className="text-3xl font-bold underline">Home Page</h1>
+    <div className="container mx-auto justify-center">
+      <Carousel
+        title='Find out what it means to become a'
+        id='CarouselTarget'
+
+      />
     </div>
   );
 };
 
 export default Main;
+
