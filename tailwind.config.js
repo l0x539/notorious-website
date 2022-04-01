14b7fa7059d62245577494406e8ca76fc7483662
l0x539 <nogues322@gmail.com>
2022-04-01 01:12:23 +0100

    update.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,20 @@
 
 module.exports = {
-    content: [
-        './pages/**/*.{js,ts,jsx,tsx}',
-        './components/**/*.{js,ts,jsx,tsx}',
-        './layout/**/*.{js,ts,jsx,tsx}',
-    ],
-    theme: {
-        fontFamily: {
-            sans: ['Rosario', 'sans-serif'],
-        },
-        extend: {
-          backgroundImage: (theme) => ({
-            nav: 'url(\'../components/assets/images/bg-image.PNG\')',
-            card: 'url(\'../components/assets/images/wood.png\')',
-          }),            
-        },
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './layout/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['Rosario', 'sans-serif'],
     },
-    plugins: [],
+    extend: {
+      backgroundImage: (theme) => ({
+        nav: 'url(\'../components/assets/images/bg-image.png\')',
+        card: 'url(\'../components/assets/images/wood.png\')',
+      }),
+    },
+  },
+  plugins: [],
 };
