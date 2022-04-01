814a4b934000d6d57ddcdff4dc2ff3aced0d6791
l0x539 <nogues322@gmail.com>
2022-04-01 03:08:45 +0100

    update.

--- components/molecules/MainHeader.tsx
+++ components/molecules/MainHeader.tsx
@@ -7,65 +7,57 @@ import skeleton from '../../assets/images/skeleton.png';
 import red from '../../assets/images/red.png';
 
 const MainHeader = () => {
-  return (<div className='flex justify-between w-full'>
+  return (<div className='grid grid-cols-[repeat(3,_1fr)] min-h-[23.875rem]'>
 
 
-    <div
-      className='flex '>
-      <img className='relative -left-24 z-20'
+    <div className='relative'>
+      <img className='absolute bottom-0 -left-24 z-20'
         src={pirate.src}
         alt="twitter"
         width='400'
         height='335'/>
-
-      <img className='relative -left-[330px] z-0'
+      <img className='absolute bottom-0 -left-[330px] z-0'
         src={skeleton.src}
         alt="twitter"
         width='500'
         height='356'
       />
-
-      <img className=' relative -left-[610px] z-10'
+      <img className=' absolute bottom-0 -left-[610px] z-10'
         src={black.src}
         alt="twitter"
         width='325'
-        height='316s'/>
+        height='316'/>
 
     </div>
 
-
-    <div>
+    <div className='flex justify-center align-center'>
       <img
         src={piratesnoto.src}
+        className="object-contain"
         alt="twitter"
         width='725'
         height='213'/>
 
     </div>
 
-    <div className='flex'>
-
-      <img className=' relative -right-[10px]'
+    <div className='relative'>
+      <img className='absolute bottom-0 -right-[10px]'
 
         src={red.src}
         alt="twitter"
         width='289'
         height='382'/>
-
-      <img className='relative -right-[10px] '
+      <img className='absolute bottom-0 -right-[10px] '
         src={red.src}
         alt="twitter"
         width='300'
         height='300'/>
-
-      <img className='relative -right-[10px]'
+      <img className='absolute bottom-0 -right-[10px]'
         src={blue.src}
         alt="twitter"
         width='425'
         height='402'/>
-
     </div>
-
   </div>);
 };
 export default MainHeader;
