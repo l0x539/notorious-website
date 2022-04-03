4cac3a652d650ecb8651e20d3cce0d8672a72d73
l0x539 <nogues322@gmail.com>
2022-04-03 22:32:11 +0100

    Adding main header.

--- components/templates/Main.tsx
+++ components/templates/Main.tsx
@@ -1,10 +1,12 @@
 
 import MainHangers from '../organisms/MainHangers';
+import MainHeader from '../organisms/MainHeader';
 import MainPosters from '../organisms/MainPosters';
 
 const Main = () => {
   return (
     <>
+      <MainHeader />
       <MainHangers />
       <MainPosters />
     </>
