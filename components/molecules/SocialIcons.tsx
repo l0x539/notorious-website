4777a9f51b3a25c19922771a0b5856e9da28ddd0
ikramakcha <akcha.ikram.a1@gmail.com>
2022-04-10 02:48:29 +0200

    Fixing SocialIcons..

--- components/molecules/SocialIcons.tsx
+++ components/molecules/SocialIcons.tsx
@@ -2,8 +2,7 @@ import SocialIcon from '../atoms/SocialIcon';
 
 const SocialIcons = ()=> {
   return (
-    <div className='lg:inline-flex lg:flex-row lg:w-auto w-full
-     lg:items-center items-start h-[89px]'>
+    <div className='grid grid-cols-3 items-center gap-2'>
       <SocialIcon type='discord' link="#" />
       <SocialIcon type='twitter' link="#" />
       <SocialIcon type='reddit' link="#" />
