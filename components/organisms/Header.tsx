d594a9b93bd847281a0050b169dca53a6ca88171
l0x539 <nogues322@gmail.com>
2022-04-25 23:02:06 +0100

    Fixing font weights.

--- components/organisms/Header.tsx
+++ components/organisms/Header.tsx
@@ -7,9 +7,7 @@ const Header = () => {
   return (
     <header>
       <div className=" bg-nav bg-cover h-[5.563rem]
-    border-b-[5px] border-[#8F8F8F]
-  border-b-shadow-[0_4px_4px_rgba(0, 0, 0, 0.25), 0_4px_4px_rgba(0, 0, 0, 0.25)]
-    ">
+      border-b-[5px] border-[#8F8F8F]">
         <nav className='container mx-auto flex items-center flex-wrap'>
           <Logo />
           <HeaderLinks />
