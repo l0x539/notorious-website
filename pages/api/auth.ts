04d3ee74dc2f65d9aa2502b9bb38768c82b75fd8
l0x539 <nogues322@gmail.com>
2022-05-29 15:09:16 +0100

    Fix static amdin name & password

--- pages/api/auth.ts
+++ pages/api/auth.ts
@@ -13,10 +13,11 @@ const Auth = async (
   const session = await getSession(req, res);
 
   if (session && !session?.loggedIn) {
-    if (req.body.name === 'pearsonwright1@gmail.com' &&
-    req.body.password === '7n8KxpUb_+<KutN') {
+    if (`${req.body.name}`.trim().toLowerCase() ===
+     process.env.ADMIN_NAME &&
+    req.body.password === process.env.ADMIN_PASSWORD) {
       session.loggedIn = true;
-      console.log('logging in', {session});
+
       return res.status(200).send({status: 'success'});
     } else return res.status(401).send({status: 'failed to login'});
   } else {
